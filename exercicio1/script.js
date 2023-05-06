f090a94f3add02fbda8b28752b7b2148f6d04992
sivilela <sivilela.s@gmail.com>
2023-05-06 09:12:35 -0300

    Exercícios dois e três incompletos

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numeroPar = Number(prompt("Digite um número par: "))
+let restoDaDivisao = numeroPar %=2
+console.log(restoDaDivisao);
+console.log(typeof numeroPar);
+console.log(typeof restoDaDivisao);
+//Como os números digitados são pares e a divisão é por 2, sempre vai restar 0.
+//Com números impares dividido por 2, sempre vai restar 1.
