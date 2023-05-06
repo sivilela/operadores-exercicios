3a1b61efdb20f0b56a05075fce708732df28b22e
sivilela <sivilela.s@gmail.com>
2023-05-06 19:43:44 -0300

    Trabalhando no exercício três e treinando git

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,4 @@ console.log(typeof numeroPar);
 console.log(typeof restoDaDivisao);
 //Como os números digitados são pares e a divisão é por 2, sempre vai restar 0.
 //Com números impares dividido por 2, sempre vai restar 1.
+//Inserido comentário apenas para testar commit
